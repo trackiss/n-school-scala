6d6ed98fabffb8b810aa0c272dd23e519341f8b3
trackiss <trackiss@hey.com>
2021-05-06 15:17:28 +0900

    応用04

---
+++ src/main/scala/advance/chapter04/Jokyu.scala
@@ -0,0 +1,20 @@
+package com.github.trackiss
+package advance.chapter04
+
+object Jokyu {
+  sealed trait Either[+E, +A] {
+    def map[B](f: A => B): Either[E, B] = this match {
+      case Right(v) => Right(f(v))
+      case Left(e)  => Left(e)
+    }
+
+    def flatMap[EE >: E, B](f: A => Either[EE, B]): Either[EE, B] = this match {
+      case Right(v) => f(v)
+      case Left(e)  => Left(e)
+    }
+  }
+
+  case class Left[+E](get: E) extends Either[E, Nothing]
+
+  case class Right[+A](get: A) extends Either[Nothing, A]
+}
