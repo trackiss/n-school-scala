d65d015c02cf5315bebaf4b68c8c4462fd680061
trackiss <trackiss@hey.com>
2021-05-06 16:10:45 +0900

    応用05

---
+++ src/main/scala/advance/chapter05/Jokyu.scala
@@ -0,0 +1,42 @@
+package com.github.trackiss
+package advance.chapter05
+
+import scala.collection.immutable.{HashMap, TreeMap}
+
+object Jokyu {
+  def foo(): Unit = {
+    var a: Array[Int] = Array.empty
+    var hm: HashMap[Int, Int] = HashMap.empty
+    var tm: TreeMap[Int, Int] = TreeMap.empty
+
+    benchmark {
+      a = (0 until 100000).toArray
+    }
+    benchmark {
+      val _ = a(99999)
+    }
+
+    benchmark {
+      hm = HashMap((0 until 100000) map (x => x -> x): _*)
+    }
+    benchmark {
+      val _ = hm(99999)
+    }
+
+    benchmark {
+      tm = TreeMap((0 until 100000) map (x => x -> x): _*)
+    }
+    benchmark {
+      val _ = tm(99999)
+    }
+  }
+
+  def benchmark(f: => Unit): Unit = {
+    val begin = System.nanoTime()
+    f
+    val end = System.nanoTime()
+
+    val formatter = java.text.NumberFormat.getNumberInstance()
+    println(s"time: ${formatter.format(end - begin)} ナノ秒")
+  }
+}
---
+++ src/main/scala/advance/chapter05/Shokyu.scala
@@ -0,0 +1,36 @@
+package com.github.trackiss
+package advance.chapter05
+
+object Shokyu {
+  def foo(): Unit = {
+    var l1: List[Int] = List.empty
+    var a1: Array[Int] = Array.empty
+    var l2: List[Int] = List.empty
+    var a2: Array[Int] = Array.empty
+
+    benchmark {
+      (0 to 10000) foreach (x => l1 = l1 :+ x)
+    }
+
+    benchmark {
+      (0 to 10000) foreach (x => a1 = a1 :+ x)
+    }
+
+    benchmark {
+      (0 to 10000) foreach (x => l2 = x +: l2)
+    }
+
+    benchmark {
+      (0 to 10000) foreach (x => a2 = x +: a2)
+    }
+  }
+
+  def benchmark(f: => Unit): Unit = {
+    val begin = System.currentTimeMillis()
+    f
+    val end = System.currentTimeMillis()
+
+    val formatter = java.text.NumberFormat.getNumberInstance()
+    println(s"time: ${formatter.format(end - begin)} ミリ秒")
+  }
+}
